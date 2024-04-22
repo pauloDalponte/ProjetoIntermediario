9c24a2a78a74fdd3eb16df45a22097636733832d
Paulo Cesar Dal Ponte <paulocesardalponte@gmail.com>
2024-04-21 23:32:32 -0300

    comentarios

--- PauloWeather/PauloWeather/src/main/java/com/paulo/PauloWeather/services/WeatherService.java
+++ PauloWeather/PauloWeather/src/main/java/com/paulo/PauloWeather/services/WeatherService.java
@@ -1,6 +1,5 @@
 package com.paulo.PauloWeather.services;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import com.paulo.PauloWeather.models.Weather;
 import com.paulo.PauloWeather.models.WeatherReturnApi;
@@ -29,14 +28,14 @@ public class WeatherService implements ConverteInfo {
                 .queryParam("key", apiKey) //insere a chave na url
                 .build())
             .retrieve()
-            .onStatus(httpStatus -> httpStatus.is4xxClientError(), response -> Mono.error(new RuntimeException("Erro no cliente ao acessar a API do terceiro.")))
-            .onStatus(httpStatus -> httpStatus.is5xxServerError(), response -> Mono.error(new RuntimeException("Erro no servidor da API do terceiro.")))            
+            .onStatus(httpStatus -> httpStatus.is4xxClientError(), response -> Mono.error(new RuntimeException("Erro no cliente ao acessar a API do terceiro."))) //verifica status da resposta http
+            .onStatus(httpStatus -> httpStatus.is5xxServerError(), response -> Mono.error(new RuntimeException("Erro no servidor da API do terceiro.")))  //verifica status da resposta http           
             .bodyToMono(WeatherReturnApi.class)
             .map(response -> response.getData().get(0))
             .onErrorResume(e -> {
-                return Mono.error(new RuntimeException("Falha ao acessa Api do Terceiro."));
+                return Mono.error(new RuntimeException("Falha ao acessa Api do Terceiro.")); //
             })
-            .block(); 
+            .block(); //aguarda retorno
     }
 }
 
